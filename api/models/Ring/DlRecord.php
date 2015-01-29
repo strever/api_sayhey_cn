ecea9223c0bd3ac5399ebd77ee15e5104185f45e
strever <fangyongxiang@appvv.com>
2015-01-29 19:56:15 +0800

    sss

--- api/models/Ring/DlRecord.php
+++ api/models/Ring/DlRecord.php
@@ -18,13 +18,10 @@ class DlRecordModel extends Mysql {
     protected static $_fields = 'ring_id,dl_time';
 
     public function modifySchema() {
-        //建表
-        //$sql = "CREATE TABLE `ring_dlrecord`( `ring_id` int(10) unsigned NOT NULL, `dltime` int(10) unsigned NOT NULL)ENGINE=MyISAM DEFAULT CHARSET=utf8";
-
-
-        //$dbh->query($sql);
-        echo "world";
         $dbh = Mysql::$dbh;
+        //建表
+        $sql = "CREATE TABLE IF NOT EXISTS ring_dlrecord( ring_id int(10) unsigned NOT NULL, dltime int(10) unsigned NOT NULL)ENGINE=MyISAM DEFAULT CHARSET=utf8";
+        $dbh->query($sql);
         //准备数据
         $stmt = $dbh->query("SELECT ring_id,download_num FROM ring WHERE download_num>100");
         $rings = $stmt->fetchAll(\PDO::FETCH_ASSOC);
@@ -32,13 +29,10 @@ class DlRecordModel extends Mysql {
             foreach(range(1,$ring['download_num']) as $i) {
                 $hour = rand(8,24);$minute = rand(0,59);$second =rand(0,59);$day = rand(1,29);$months = array('2014-11','2014-12','2015-01');$month = $months[array_rand($months)];
                 $dlTime = strtotime("$month-$day $hour:$minute:$second");
-                echo $sql = "INSERT INTO ring_dlrecord(ring_id,dltime) VALUES($ring[ring_id],$dlTime)" . "<br>";
+                $sql = "INSERT INTO ring_dlrecord(ring_id,dltime) VALUES($ring[ring_id],$dlTime)" . "<br>";
+                $dbh->exec($sql) or die(print_r($dbh->errorInfo(), true));
             }
         }
-
-
-
-
     }
 }
  
